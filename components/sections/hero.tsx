dc64b4ffde593002456d1671c811c7e74e4fdd1f
vanderms <vanderms.84@gmail.com>
2022-02-08 20:47:35 -0300

    add media tablet to hero section

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,20 +1,29 @@
 /* eslint-disable @next/next/no-img-element */
-import path from "../../lib/path"
+import path from '../../lib/path';
 
-
-
-export default function Hero(){
-  
+export default function Hero() {
   return (
     <section className="hero-section">
-      <div className="background-details">
-        <div className="details details-1"></div>
-        <div className="details oval-2"></div>
-        <div className="details oval-3"></div>
-      </div>      
-      <img src={path('/assets/logo.svg')} alt="equalizer logo" className="logo" />
-      <img src={path('/')} alt="" className="detail-1" />
-
+      <div className="background-images">
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="details"></div>
+      </div>
+      <img
+        src={path('/assets/logo.svg')}
+        alt="equalizer logo"
+        className="logo"
+      />
+      <header className='header'>
+        <h1 className="title">We make your music sound extraordinary.</h1>
+        <p className="description">
+          A system audio equalizer specifically designed for Android and iOS.
+          Freely tune the way your music sounds with a professional grade
+          parametric EQ & volume mixer. Control bass, mids, treble, gain control,
+          reverb, and more!
+        </p>
+      </header>      
     </section>
-  )
+  );
 }
